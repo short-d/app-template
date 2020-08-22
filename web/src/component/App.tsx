74a8ce2c7adf009deff586823b283cb857b95f28
Harry Liu <byliuyang11@gmail.com>
2020-08-21 17:38:34 -0700

    Hook up change log API with frontend

--- web/src/component/App.tsx
+++ web/src/component/App.tsx
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styles from './App.module.scss';
+import {SampleAppGraphqlService} from '../service/sampleapp.graphql.service';
+import {Change} from '../entity/change';
 
-function App() {
-  return <div className={styles.App}>Frontend works!</div>;
+interface IProps {
+  sampleAppGraphQLService: SampleAppGraphqlService;
+}
+
+interface IProps {
+  sampleAppGraphQLService: SampleAppGraphqlService;
+}
+
+interface IState {
+  changes: Change[];
+}
+
+export class App extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      changes: []
+    };
+  }
+
+  async componentDidMount() {
+    const changeLog = await this.props.sampleAppGraphQLService.getChangeLog();
+    this.setState({
+      changes: changeLog.changes
+    });
+  }
+
+  render() {
+    return (
+      <div className={styles.App}>
+        <h1>Frontend Works!</h1>
+        <h2>Change Log</h2>
+        <ul className={styles.ChangeLog}>
+          {this.state.changes.map(change =>
+            <li>
+              {`${change.title}`}
+            </li>
+          )}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './component/App';
 import * as serviceWorker from './serviceWorker';
+import {GraphQLService} from './service/fw/GraphQL.service';
+import {FetchHTTPService} from './service/fw/http.service';
+import {SampleAppGraphqlService} from './service/sampleapp.graphql.service';
+import {EnvService} from './service/env.service';
+
+const envService = new EnvService();
+const fetchHTTPService = new FetchHTTPService();
+const graphQLService = new GraphQLService(fetchHTTPService);
+const sampleAppGraphQLService = new SampleAppGraphqlService(graphQLService, envService);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App sampleAppGraphQLService={sampleAppGraphQLService}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
